68b35cbd20accabb145b44fb2718af60e98afb1b
rmsmartins <52045144+rmsmartins@users.noreply.github.com>
2022-07-27 03:35:10 +0100

    criar @PostMapping

--- src/main/java/com/rmsmartins/linguagens/api/LinguagemController.java
+++ src/main/java/com/rmsmartins/linguagens/api/LinguagemController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -17,6 +19,12 @@ public class LinguagemController {
     public List<Linguagem> obterLinguagens() {     
         List<Linguagem> linguagens = repositorio.findAll();
         return linguagens;   
+    }
+
+    @PostMapping("/linguagens")
+    public Linguagem adicionarLinguagem(@RequestBody Linguagem linguagem) {
+        Linguagem linguagemSalva = repositorio.save(linguagem);
+        return linguagemSalva;
     }
     
 }
