6455a3fa4db91df9ecb65a3d62ba8a79ee8555bf
rmsmartins <52045144+rmsmartins@users.noreply.github.com>
2022-07-27 01:19:15 +0100

    Acrescentar linguagens

--- src/main/java/com/rmsmartins/linguagens/api/LinguagemController.java
+++ src/main/java/com/rmsmartins/linguagens/api/LinguagemController.java
@@ -12,7 +12,9 @@ public class LinguagemController {
     private List<Linguagem> linguagens = 
         List.of(
             new Linguagem("Java", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/java/java_256x256.png", 1),
-            new Linguagem("PHP", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/php/php_256x256.png", 2)
+            new Linguagem("PHP", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/php/php_256x256.png", 2),
+            new Linguagem("Javascript", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/javascript/javascript_256x256.png", 3),
+            new Linguagem("Python", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/python/python_256x256.png", 4)
         );
 
 
