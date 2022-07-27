d942aa1f8fb47b2a6c6ef300a0179d619028970e
rmsmartins <52045144+rmsmartins@users.noreply.github.com>
2022-07-27 03:08:10 +0100

    remover a lista de linguagens

--- src/main/java/com/rmsmartins/linguagens/api/LinguagemController.java
+++ src/main/java/com/rmsmartins/linguagens/api/LinguagemController.java
@@ -9,13 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LinguagemController {
     
-    private List<Linguagem> linguagens = 
-        List.of(
-            new Linguagem("Java", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/java/java_256x256.png", 1),
-            new Linguagem("PHP", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/php/php_256x256.png", 2),
-            new Linguagem("Javascript", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/javascript/javascript_256x256.png", 3),
-            new Linguagem("Python", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/python/python_256x256.png", 4)
-        );
+  
+ 
 
 
     @GetMapping("/linguagens")
