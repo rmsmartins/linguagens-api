497aea138c05beec65cfed3f05d1f99c6967d6d5
rmsmartins <52045144+rmsmartins@users.noreply.github.com>
2022-07-27 03:11:50 +0100

    obter dados da base de dados

--- src/main/java/com/rmsmartins/linguagens/api/LinguagemController.java
+++ src/main/java/com/rmsmartins/linguagens/api/LinguagemController.java
@@ -2,6 +2,7 @@ package com.rmsmartins.linguagens.api;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,12 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LinguagemController {
     
-  
- 
-
+    @Autowired
+    private LinguagemRepository repositorio;
 
     @GetMapping("/linguagens")
     public List<Linguagem> obterLinguagens() {     
+        List<Linguagem> linguagens = repositorio.findAll();
         return linguagens;   
     }
     
